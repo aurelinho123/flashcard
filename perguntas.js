fea837e7c6f4f524f17148da485ea7d97fdb1704
aurelinho123 <xarulino@gmail.com>
2024-11-13 10:57:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Tiro com Arco',
+    'O que é o "target" no tiro com arco?',
+    'O "target" é o alvo no qual o arqueiro tenta atingir com suas flechas'
+)
+
+criaCartao(
+    'Tiro com Arco',
+    'Qual a distância padrão para as competições de tiro com arco?',
+    'A distância padrão para competições de tiro com arco varia, mas para arco recurvo é 70 metros para os homens e 70 metros para as mulheres na maioria das competições internacionais'
+)
+
+criaCartao(
+    'Tiro com Arco',
+    'O que é o "arco recurvo"?',
+    'O arco recurvo é um tipo de arco com as extremidades curvadas para fora, o que proporciona maior potência e velocidade nas flechas'
+)
+
+criaCartao(
+    'Tiro com Arco',
+    'Qual a função da empunhadura no arco?',
+    'A empunhadura do arco é a parte que o arqueiro segura, e sua função é proporcionar estabilidade e controle durante o disparo'
+)
